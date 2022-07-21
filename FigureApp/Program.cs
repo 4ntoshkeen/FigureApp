90df1368489f2f1c741a270096efbc876a32003c
Antoshkeen <g4zefstronof@gmail.com>
2022-07-22 01:08:47 +0400

    Add project files.

---
+++ FigureApp/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+using FigureLibrary;
+
+namespace FigureApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /******************  ВВОДИМ КОЛИЧЕСТВО УГЛОВ ФИГУРЫ  ******************/
+
+            Console.WriteLine("HOW MANY ANGLES?: ");                                           
+            string angle = Console.ReadLine();
+
+
+            /******************  ПРОВЕРЯЕМ, ВСЕ ЛИ В ПОРЯДКЕ  СО ВВОДОМ  ******************/
+            try
+            {
+                int angleNumber = int.Parse(angle);
+
+                // СЛИШКОМ МНОГО УГЛОВ                                
+                if (angleNumber > 3)  
+                {
+                    Console.WriteLine("TOO MUCH ANGLES. NEED TO ADD THIS FUGURE IN YOUR PROGRAM.");
+                }
+
+                // ЧИСЛО УГЛОВ НЕ ДОЛЖНО БЫТЬ ОТРИЦАТЕЛЬНЫМ
+                else if (angleNumber < 0)
+                {
+                    Console.WriteLine("CAN'T BE NEGATIVE.");
+                }
+
+                else if (angleNumber == 1 || angleNumber == 2)
+                {
+                    Console.WriteLine("NEED MORE ANGLES.");
+                }
+
+                // В СЛУЧАЕ ОКРУЖНОСТИ
+                else if (angleNumber == 0)                                                                     
+                {
+                    Console.WriteLine("CIRCLE? ENTER RADIUS HERE: ");
+                    string radius = Console.ReadLine();
+                    
+                    // ПРОВЕРКА ЗНАЧЕНИЯ РАДИУСА 
+                    try
+                    {                        
+                        double radiusNumber = double.Parse(radius);
+                        Figure figure = new Figure();
+
+                        
+                        if (radiusNumber < 0)
+                        {
+                            Console.WriteLine("CAN'T BE NEGATIVE.");
+                        }
+
+
+                        //ВЫЧИСЛЯЕМ ПЛОЩАДЬ И ВЫВОДИМ В КОНСОЛИ
+                        else
+                        {
+                            Console.WriteLine("AREA IS: " + figure.Area(radiusNumber));                            
+                        }
+                    }
+
+                    catch
+                    {
+                        Console.WriteLine($"UNABLE TO PARSE '{radius}'");
+                    }
+                }
+
+
+                else
+                {
+                    // ВВОДИМ ЗНАЧЕНИЯ СТОРОН
+
+                    Console.WriteLine("ENTER SIDES OF TRIANGLE HERE: ");
+                    string A = Console.ReadLine();
+                    string B = Console.ReadLine();
+                    string C = Console.ReadLine();
+
+                    // ПРОВЕРКА ЗНАЧЕНИЙ 
+                    try
+                    {
+                        int a = int.Parse(A);
+                        int b = int.Parse(B);
+                        int c = int.Parse(C);
+                        Figure figure = new Figure();
+
+
+                        if (a <= 0 || b <= 0 || c <= 0 )
+                        {
+                            Console.WriteLine("SIDE CAN'T BE NULL OR NEGATIVE.");
+                        }
+
+
+                        //ВЫЧИСЛЯЕМ ПЛОЩАДЬ И ВЫВОДИМ В КОНСОЛИ
+                        else
+                        {
+                            Console.WriteLine(figure.Area(a, b, c));
+                        }
+                    }
+
+                    catch
+                    {
+                        Console.WriteLine($"UNABLE TO PARSE");
+                    }
+
+                }
+
+                
+            }
+
+            /******************  ВЫДАЕМ ОШИБКУ КОНВЕРТАЦИИ  ******************/
+
+            catch (FormatException)
+
+            {
+                Console.WriteLine($"UNABLE TO PARSE '{angle}'");
+            }
+
+        }
+    }
+}
---
+++ FigureLibrary/Figure.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace FigureLibrary
+{
+
+    /* 
+     
+     ОБЪЯВЛЯЕМ КЛАСС 'FIGURE'. В НЕМ СОДЕРЖАТСЯ ПОЛЯ ДЛЯ ЗНАЧЕНИЙ СТОРОН ФИГУРЫ И ПОЛЕ РАДИУСА (ПО УМОЛЧАНИЮ РАВНОЕ НУЛЮ). 
+     ДЛЯ БОЛЬШЕГО КОЛИЧЕСТВА УГЛОВ НЕОБХОДИМО ДОБАВИТЬ СООТВЕТСТВУЮЩЕЕ ПОЛЕ ДОПОЛНИТЕЛЬНЫМ СТОРОНАМ.   
+     ТАКЖЕ НЕОБХОДИМО ДОПОЛНИТЕЛЬНО СОЗДАТЬ МЕТОД ДЛЯ ВЫЧИСЛЕНИЯ ПЛОЩАДИ ФИГУРЫ, НО С БОЛЬШИМ КОЛИЧЕСТВОМ ПЕРЕДАВАЕМЫХ ПАРАМЕТРОВ.
+          
+     */
+
+    public class Figure
+    {
+        public int a;
+        public int b;
+        public int c;
+        // ЗДЕСЬ МОГУТ БЫТЬ ДОПОЛНИТЕЛЬНЫЕ ПОЛЯ
+        public double r = 0;
+
+
+        // МЕТОД ДЛЯ ОКРУЖНОСТИ
+        public double Area(double r)
+        {
+            const double pi = 3.1415926535897931;
+            double result = pi * (r * r);
+            return result;  
+        }
+
+        // МЕТОД ДЛЯ ТРЕУГОЛЬНИКА
+        public double Area(int a, int b, int c)
+        {
+            double semifPerimeter = (a + b + c) / 2;
+            double result = Math.Sqrt(semifPerimeter * (semifPerimeter - a) * (semifPerimeter - b) * (semifPerimeter - c));   
+            return result;
+        }
+
+    }
+}
